0a276820e5d679db5920b06915c5af223e8f5cec
yu-M215 <yuu0609km.215@gmail.com>
2022-09-13 00:36:58 +0900

    トランスコンパイルする

---
+++ sample.js
@@ -0,0 +1,4 @@
+window.addEventListener('load', function (event) {
+    var p = document.querySelector('#target');
+    p.textContent = "This is message by TypeScript.";
+});
---
+++ sample.ts
@@ -0,0 +1,4 @@
+window.addEventListener('load', (event)=>{
+  let p = document.querySelector('#target')
+  p.textContent = "This is message by TypeScript."
+})
